749e44b0773464eeb55415a8d12ac18bf4527402
Sinan <sinankara@gmail.com>
2021-02-09 10:40:35 +0300

    Initial Commit

---
+++ GradientView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GradientView
+//
+//  Created by Sinan Kara on 9.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
